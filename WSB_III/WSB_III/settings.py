6023b23e354805d5cd1eeee357e6c7cbf5657aaa
KacperTyczynski <43890890+KacperTyczynski@users.noreply.github.com>
2021-02-25 22:06:24 +0100

    Próba dodania plików z folderu Stati (FAIL)

--- WSB_III/WSB_III/settings.py
+++ WSB_III/WSB_III/settings.py
@@ -120,3 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#NIE DZIAŁA    
+# STATICFILES_DIR = [
+  #  Path('/WSB_III/static').resolve().parent.parent
+#]
