2a171efbe2f385b4d35c835eae13aa84c4247f23
KacperTyczynski <43890890+KacperTyczynski@users.noreply.github.com>
2021-03-03 13:49:39 +0100

    dodanie iframe

--- WSB_III/RSS_Page/urls.py
+++ WSB_III/RSS_Page/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', views.home),
     path('zaplecze/', views.zaplecze),
+    path('RSS/', TemplateView.as_view(template_name="RSS.html"),
+                   name='RSS'),
 ]
